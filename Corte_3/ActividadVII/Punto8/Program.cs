eb2a437e844a6d07ab123758bcc9d72ffa53fbac
Miguel <m.araque.contacto@gmail.com>
2021-10-17 22:23:29 -0500

    upload: Punto8: Intercambio de Colas

---
+++ Corte_3/ActividadVII/Punto8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Punto8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Worl!");
+        }
+        static void requestNumbers(Stack Numbers, int Total)
+        {
+            Random RNumber = new Random();
+            for(int y = 0; y < Total; y++)
+            {
+                Console.Write($"\nEscribe el numero {y + 1}: ");
+                int Value = int.Parse(System.Console.ReadLine());
+                Numbers.Push(Value);
+            }
+        }
+    }
+}
