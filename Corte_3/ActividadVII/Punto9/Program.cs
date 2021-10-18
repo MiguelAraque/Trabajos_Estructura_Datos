a95c50b74ac6bb848b8f844b8e655ea6eb7f6565
Miguel <m.araque.contacto@gmail.com>
2021-10-18 12:19:59 -0500

    upload: Punto 9: clonar Cola

---
+++ Corte_3/ActividadVII/Punto9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+
+namespace Punto9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Queue1 = new Queue();
+            Queue Queue2 = new Queue();   
+            requestNumbers(Queue1, 3);
+            CloneQueue(Queue1, Queue2);
+            Console.WriteLine("Cola orginal");
+            printQueueValues(Queue1);
+            Console.WriteLine("Cola Clonada");
+            printQueueValues(Queue2);
+        }
+
+        static void requestNumbers(Queue Numbers, int Total)
+        {
+            Random RNumber = new Random();
+            for(int y = 0; y < Total; y++)
+            {
+                Numbers.Enqueue(RNumber.Next(1,10));
+            }
+        }
+
+        static void CloneQueue(Queue originalQueue, Queue clonedQueue)
+        {
+            foreach (var item in originalQueue)
+            {
+                clonedQueue.Enqueue(item);
+            }
+        }
+
+        static void printQueueValues(Queue queue)
+        {
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
